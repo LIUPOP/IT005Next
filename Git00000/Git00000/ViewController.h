ecc9a6d8d58ac133c0a571946d9a8bd602291669
QrCodeScanDemo001 <student@liubo.local>
2016-06-01 10:10:06 +0800

    liu
    addnum

---
+++ Git00000/Git00000/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Git00000
+//
+//  Created by student on 16/6/1.
+//  Copyright © 2016年 POP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
